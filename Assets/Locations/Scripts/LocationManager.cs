dcf8d2be1a807151b7aed11e75c25357d3e8e572
josschi95 <joeschiller95@gmail.com>
2024-02-15 01:35:48 -0500

    fuck

--- Assets/Locations/Scripts/LocationManager.cs
+++ Assets/Locations/Scripts/LocationManager.cs
@@ -7,21 +7,20 @@ namespace SD.LocationSystem
     {
         [SerializeField] private LocationDatabase database;
 
-        [Space]
-
-        [SerializeField] private LocationEncounter locationPrefab;
+        //[SerializeField] private LocationEncounter locationPrefab;
 
         private void Awake() => database.Init();
         private void Start() => PlaceLocations();
 
         private void PlaceLocations()
         {
+            /*
             foreach(var location in database.GetAllLocations())
             {
                 var newLocation = Instantiate(locationPrefab);
                 newLocation.SetLocation(location);
                 newLocation.transform.SetParent(transform);
-            }
+            }*/
             //will also need to include IsDiscovered values for all presets
 
             //work this in later
